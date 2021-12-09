b9f2cf32b4b8bb0a0cc47f4b32e2a534c7be136a
unknown <shodiyatimilehin@gmail.com>
2021-12-09 14:50:09 +0100

    Fixed some bugs. All .api/minions routes tests now passing

--- server/minions-api.js
+++ server/minions-api.js
@@ -18,10 +18,10 @@ router.get('/:minionId', (req, res, send) => {
 
 router.post('/', (req, res, next) => {
     const newMinion = req.body;
-    if (typeof newMinion.name !== 'string' || typeof newMinion.title !== 'string' || typeof newMinion.weaknesses !== 'string' || !newMinion.salary) {
+    if (typeof newMinion.name !== 'string' || typeof newMinion.title !== 'string' || typeof newMinion.weaknesses !== 'string') {
         res.status(404).send('bad request');
     } else {
-        res.send(db.addToDatabase('minions', req.body));
+        res.status(201).send(db.addToDatabase('minions', req.body));
     };
 });
 
@@ -44,11 +44,11 @@ router.put('/:minionId', (req, res, next) => {
 router.delete('/:minionId', (req, res, next) => {
     const id = req.params.minionId;
     
-    if (!db.deleteFromDatabasebyId('minions', id)) {
-        res.status(404).send("Not found")
+    if (db.deleteFromDatabasebyId('minions', id) !== true) {
+        res.status(404).send("Not found");
     } else {
-        res.send('Deleted');
-    }
+        res.status(204).send('Deleted');
+    };
 });
 
 
