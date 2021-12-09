102de1266e01d73adce47adae3687f3fca75674f
unknown <shodiyatimilehin@gmail.com>
2021-12-09 14:18:24 +0100

    Minions api router completed, functional.

--- server/minions-api.js
+++ server/minions-api.js
@@ -7,6 +7,15 @@ router.get('/', (req, res, next) => {
     res.status(200).send(minions);
 });
 
+router.get('/:minionId', (req, res, send) => {
+    const id = req.params.minionId;
+    if (db.getFromDatabaseById("minions", id) == null) {
+        res.status(404).send('Not found')
+    } else {
+        res.send(db.getFromDatabaseById("minions", id))
+    };
+})
+
 router.post('/', (req, res, next) => {
     const newMinion = req.body;
     if (typeof newMinion.name !== 'string' || typeof newMinion.title !== 'string' || typeof newMinion.weaknesses !== 'string' || !newMinion.salary) {
@@ -14,6 +23,32 @@ router.post('/', (req, res, next) => {
     } else {
         res.send(db.addToDatabase('minions', req.body));
     };
+});
+
+router.put('/:minionId', (req, res, next) => {
+    const id = req.params.minionId;
+    const updatedMinion = req.body;
+    updatedMinion.id = id;
+    if (db.getFromDatabaseById("minions", id) == null) {
+        res.status(404).send('Not found');
+    } else {
+        updatedMinion.id = id;
+        try {
+            res.send(db.updateInstanceInDatabase('minions', updatedMinion));
+        } catch (err) {
+            next(err)
+        };
+    };
+});
+
+router.delete('/:minionId', (req, res, next) => {
+    const id = req.params.minionId;
+    
+    if (!db.deleteFromDatabasebyId('minions', id)) {
+        res.status(404).send("Not found")
+    } else {
+        res.send('Deleted');
+    }
 });
 
 
